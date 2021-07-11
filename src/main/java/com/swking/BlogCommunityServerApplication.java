d4da35dd9fca34725c4dd0986ecacec39760a23f
Swking <1114006175@qq.com>
2021-07-11 13:31:09 +0800

    init repo

---
+++ src/main/java/com/swking/BlogCommunityServerApplication.java
@@ -0,0 +1,13 @@
+package com.swking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCommunityServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogCommunityServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swking/BlogCommunityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCommunityServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
